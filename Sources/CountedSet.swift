01cb8442ab1343814c00082fa1b95999535ab96e
Matthias Lamoureux <letatas@gmail.com>
2017-01-29 10:44:10 +0100

    Cleaning

--- Sources/CountedSet.swift
+++ Sources/CountedSet.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2015 0x7fffffff. All rights reserved.
 //
 
-public struct CountedSet<T: Hashable>: SetAlgebra, ExpressibleByArrayLiteral {
+public struct CountedSet<T: Hashable>: SetAlgebra {
     public typealias Element = T
 
     fileprivate var backingDictionary = [Element: Int]()
